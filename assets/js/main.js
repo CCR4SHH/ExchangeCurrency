6afee2d6666a1ad91512bb28615df726bd5ac07d
Muhammed Meraki <muhammedm2597@gmail.com>
2023-05-26 14:18:38 +0300

    All Currency Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 var currency = ["USD", "JPY", "DKK", "EUR", "GBP", "NOK"];
 
 
-
 function jsondata($base){
   var requestURL = `https://api.exchangerate.host/latest?base=${$base}`; 
   var request = new XMLHttpRequest(); 
@@ -34,6 +33,56 @@ function excdata(){
   }
 }
 
+function allcurrency() {
+  var requestURL = 'https://api.exchangerate.host/symbols'; 
+  var request = new XMLHttpRequest(); 
+  request.open('GET', requestURL);
+  request.responseType = 'json';
+  request.send();
+
+  request.onload = function() {
+    var response = request.response;
+
+    $.each(response.symbols, function(key, value) {
+      allCurrencyValue(value.code, value.description);
+    });
+  }
+}
+
+function allCurrencyValue(symbol, desc) {
+  var requestURL = `https://api.exchangerate.host/latest?base=${symbol}`; 
+      var request = new XMLHttpRequest(); 
+      request.open('GET', requestURL);
+      request.responseType = 'json';
+      request.send();
+    
+      request.onload = function() {
+        var response = request.response;
+        curHtml(symbol, desc, response.rates["TRY"]);
+      }
+}
+
+function curHtml(code, desc, rate){
+  rate = rate.toFixed(4);
+  $("#allcurrency .column").append(`
+                    <div class="currency">
+                        <img src="assets/images/flags/${code}.png" alt="">
+                        <div class="cur-name">
+                            <span class="code">${code}</span>
+                            <span class="name">${desc}</span>
+                        </div>
+                        <div class="cur-buy">
+                            <span class="buy-sell">ALIŞ</span>
+                            <span class="cur-value">${rate}</span>
+                        </div>
+                        <div class="cur-sell">
+                            <span class="buy-sell">SATIŞ</span>
+                            <span class="cur-value">${rate}</span>
+                        </div>
+                    </div>
+                    `);
+}
+
 $(document).ready(function () {
   $.each(currency, function (index, value) { 
     jsondata(value);
@@ -42,7 +91,12 @@ $(document).ready(function () {
   $("#curtype").change(function (e) { 
     excdata();
   });
+
   $("#curexc").change(function (e) { 
     excdata();
+  });
+
+  $(".btn-all").click(function (e) { 
+    allcurrency();
   });
 });
